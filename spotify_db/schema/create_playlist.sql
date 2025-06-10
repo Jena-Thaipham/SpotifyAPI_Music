991ac3c833e893b50dde57e84a763f93d98d98cc
ThaiPham-Jena <166684763+Jena-Thaipham@users.noreply.github.com>
2025-06-10 09:51:14 -0700

    Delete spotify_db directory

--- spotify_db/schema/create_playlist.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `playlists` (
-    `playlist_id` TEXT PRIMARY KEY,
-    `playlist_name` TEXT,
-    `owner_id` TEXT,
-    `total_tracks` INTEGER,
-    `public` BOOLEAN,
-    `playlist_uri` TEXT,
-    FOREIGN KEY (`owner_id`) REFERENCES `users`(`user_id`)
-);
--- spotify_db/schema/create_user.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-            `user_id` TEXT PRIMARY KEY,
-            `followers` INTEGER,
-            `user_uri` TEXT
-);
